c730b13d53e810d764cc1a1925d2d91c8bfccebb
Brayam Teofilo Jimenez <brateomx@gmail.com>
2023-02-07 16:46:58 -0600

    Primer commit

---
+++ footer.js
@@ -0,0 +1,10 @@
+document.write(`
+<!-- footer section -->
+  <section class="container-fluid footer_section">
+    <p>
+      &copy; 2023 All Rights Reserved By
+      <a href="https://google.com.mx/">Brayam Teofilo Jimenez (Editando)</a>
+    </p>
+  </section>
+  <!-- footer section -->
+  `);
---
+++ head.js
@@ -0,0 +1,31 @@
+document.write(`
+<head>
+    <!-- Basic -->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <!-- Site Metas -->
+    <meta name="keywords" content="BraTeoMX, brateomx, portafolio de brayam, brayam, " />
+    <meta name="description" content="El portafolio de Brayam tiene el objetivo de mostrar proyectos personales y de trabajos" />
+    <meta name="author" content="Brayam Teofilo Jimenez" />
+    
+  
+    <title>ELEMENTS</title>
+  
+    <!-- slider stylesheet -->
+    <link rel="stylesheet" type="text/css"
+      href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.1.3/assets/owl.carousel.min.css" />
+  
+    <!-- bootstrap core css -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+  
+    <!-- fonts style -->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:400,700|Roboto:400,700&display=swap" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet" />
+    <!-- responsive style -->
+    <link href="css/responsive.css" rel="stylesheet" />
+  </head>
+
+  `);
